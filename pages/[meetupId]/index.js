28f075c79923628dab13bd16bc003368197633fd
FathurAnshari <anshariF17@gmail.com>
2022-05-24 22:11:50 +0700

    MeetupDetails page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,1 +1,15 @@
 // our-domain.com/indentifier
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://corporette.com/wp-content/uploads/2017/03/best-places-for-informal-business-meetings-for-women-lawyers-consultants-and-more.jpg"
+      title="The First Meetup"
+      address="Some Address 5 , some city"
+      description="This is the first meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
