752eea961401fdce20503808fbca85ea98a10b77
FathurAnshari <anshariF17@gmail.com>
2022-05-25 13:58:12 +0700

    create API and mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,26 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 // POST /api/new-meetup
 
-const handler = (req, res) => {
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, description, address } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://fathuranshari:E0PFo1PNiuS686fW@cluster0.z0txn.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted" });
   }
 };
 
